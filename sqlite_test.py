65fb4146118a19f91f19e9bd1a8435c7d7e9ee65
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-07-16 15:44:20 -0500

    Initial commit

--- sqlite_test.py
+++ sqlite_test.py
@@ -1,4 +1,5 @@
 import sqlite3
+
 from employee import Employee
 
 con = sqlite3.connect('employee.db')
@@ -12,11 +13,11 @@ c = con.cursor()
 emp1 = Employee('Sujal', 'Shrestha', '50000')
 emp2 = Employee('Rajen', 'Bajgain', '60000')
 
-c.execute("INSERT INTO employees VALUES ('Sujal', 'Shrestha', 50000)")
+c.execute("INSERT INTO employees VALUES (?, ?, ?)", (emp1.first, emp1.last, emp1.pay))
 
 con.commit()
 
-c.execute("INSERT INTO employees VALUES (?, ?, ?)", (emp1.first, emp1.last, emp1.pay))
+c.execute("INSERT INTO employees VALUES (:first, :last, :pay)", {'first': emp2.first, 'last': emp2.last, 'pay': emp2.pay})
 
 con.commit()
 
