caa462611d9da5da989098526aaf9813f0109d40
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-07-16 11:16:38 -0500

    Initial commit

--- sqlite_test.py
+++ sqlite_test.py
@@ -1,1 +1,12 @@
 import sqlite3
+
+con = sqlite3.connect('employee.db')
+c = con.cursor()
+c.execute("""CREATE TABLE employees (
+            first text,
+            last text,
+            pay int,
+    )""")
+
+con.commit()
+con.close()
