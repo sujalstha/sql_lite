4a5ad053925f2d93d29741c845babac1f5df6554
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-07-16 11:23:49 -0500

    Initial commit

--- sqlite_test.py
+++ sqlite_test.py
@@ -11,4 +11,10 @@ c.execute("""CREATE TABLE employees (
 c.execute("INSERT INTO employees VALUES ('Sujal', 'Shrestha', 50000)")
 
 con.commit()
+
+c.execute("SELECT * FORM employees WHERE last='Shrestha' ")
+
+print(c.fetchall())
+
+con.commit()
 con.close()
