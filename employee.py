5c6a23808844ae42c10f73296b889a372f372b27
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-07-16 11:09:43 -0500

    Initial commit

--- employee.py
+++ employee.py
@@ -11,3 +11,9 @@ class Employee:
     @property
     def full_name(self):
         return '{} {}'.format(self.first, self.last)
+
+    @property
+    def emp_pay(self):
+        tax_cut = self.pay/.05
+        self.pay = self.pay-tax_cut
+        return '{} {} pay is {}'.format(self.first, self.last, self.pay)
