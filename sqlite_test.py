314918bb0a8a07587358a02e6e97703fb1e7df29
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-07-16 21:59:15 -0500

    Initial commit

--- sqlite_test.py
+++ sqlite_test.py
@@ -4,6 +4,7 @@ from employee import Employee
 
 con = sqlite3.connect('employee.db')
 c = con.cursor()
+
 '''c.execute("""CREATE TABLE employees (
             first text,
             last text,
@@ -13,13 +14,14 @@ c = con.cursor()
 emp1 = Employee('Sujal', 'Shrestha', '50000')
 emp2 = Employee('Rajen', 'Bajgain', '60000')
 
-c.execute("INSERT INTO employees VALUES (?, ?, ?)", (emp1.first, emp1.last, emp1.pay))
+# c.execute("INSERT INTO employees VALUES (?, ?, ?)", (emp1.first, emp1.last, emp1.pay))
 
-con.commit()
+# con.commit()
 
-c.execute("INSERT INTO employees VALUES (:first, :last, :pay)", {'first': emp2.first, 'last': emp2.last, 'pay': emp2.pay})
+# c.execute("INSERT INTO employees VALUES (:first, :last, :pay)",
+#          {'first': emp2.first, 'last': emp2.last, 'pay': emp2.pay})
 
-con.commit()
+# con.commit()
 
 c.execute("SELECT * FROM employees WHERE last='Shrestha' ")
 
