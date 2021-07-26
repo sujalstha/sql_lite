55e761c59af8457fe98fb0240f687182f6ea2cc0
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-07-26 17:27:31 -0500

    Initial commit

--- first-class-function.py
+++ first-class-function.py
@@ -1,13 +1,11 @@
 def html_tag(tag):
-
     def wrap_text(msg):
         print('{0}><{1}></{0}>'.format(tag, msg))
 
     return wrap_text
 
 
-print_h1 = html_tag('h1')
+# print_h1 = html_tag('h1')
+print_h1 = html_tag('p')
 print_h1('Test Headline!')
 print_h1('Another Headline!')
-
-
