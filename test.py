76459f5ed40e36f05ed04ea9a24f626486e78976
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-08-02 17:14:34 -0500

    Initial commit

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ while str(input('Enter YES if you are ready: ')):
     t1 = time.time()
     input_text = len(str(f'Type the phrase: "{word}" as fast as you can.'))
     t2 = time.time()
-    accuracy = len(set(input_text.split() & word.split()))
+    accuracy = len(set(input_text.split & word.split()))
     accuracy = accuracy/word_count
     time_taken = t1 - t2
     wordPM =  word_count/time_taken
