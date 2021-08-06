10fa057c95afe477c553d066d156a26b01e64583
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-08-05 22:37:59 -0500

    Initial commit

--- test.py
+++ test.py
@@ -14,4 +14,17 @@ goto(-100, 205)
 color("white")
 write("TURTLE RACE", font=("Arial", 20, "bold"))
 
+# Track
+goto(-350, 200)
+pendown()
+color("chocolate")
+begin_fill()
+
+for i in range(2):
+    forward(700)
+    right(90)
+    forward(400)
+    right(90)
+end_fill()
+
 mainloop()
