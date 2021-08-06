15cd091e9e9a0129e2ca23baa1c2e7f0933f4dd1
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-08-05 22:28:50 -0500

    Initial commit

--- test.py
+++ test.py
@@ -1,16 +1,5 @@
-import time
-import random
+import turtle
+from turtle import *
 
-words_list = ["suburban", "assuming", "foramens", "obsidian", "Science"]
-word = random.choice(words_list)
-word_count = len(word.split())
-
-while str(input('Enter YES if you are ready: ')):
-    t1 = time.time()
-    input_text = len(str(f'Type the phrase: "{word}" as fast as you can.'))
-    t2 = time.time()
-    accuracy = len(set(input_text.split() & word.split()))
-    accuracy = accuracy/word_count
-    time_taken = t1 - t2
-    wordPM = word_count/time_taken
-    print(wordPM, accuracy, time_taken)
+setup(800, 900)
+mainloop()
